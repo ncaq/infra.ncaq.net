48a62348c8a4c8ed82e50de138d0fc14f2aa9207
ncaq <ncaq@ncaq.net>
2025-10-26 12:57:38 +0900

    feat(cloudflare): forgejo用のアクセスポイントを追加

---
+++ cloudflare/forgejo.tf
@@ -0,0 +1,21 @@
+resource "cloudflare_dns_record" "forgejo" {
+  zone_id = var.zone_id
+  name    = "forgejo.ncaq.net"
+  type    = "CNAME"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.seminar.id}.cfargotunnel.com"
+  proxied = true
+  ttl     = 1
+}
+
+# ssh接続でcloneやpushを行うためのアクセスポイントです。
+# 無料版のCloudflare Zeno Trust Tunnelのhttpsでの一回での通信では転送量制限が厳しいのでsshに逃げられるようにしています。
+# 無料版のCloudflare Zeno Trust Tunnelの制約上`ssh.forgejo.ncaq.net`とするとTLSの解決ができないため、
+# `forgejo-ssh.ncaq.net`で妥協しています。
+resource "cloudflare_dns_record" "forgejo-ssh" {
+  zone_id = var.zone_id
+  name    = "forgejo-ssh.ncaq.net"
+  type    = "CNAME"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.seminar.id}.cfargotunnel.com"
+  proxied = true
+  ttl     = 1
+}
