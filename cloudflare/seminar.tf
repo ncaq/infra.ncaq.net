4b186c678ca40be38593c9d4870c5b11c8ce3f1d
ncaq <ncaq@ncaq.net>
2026-01-03 09:07:12 +0900

    feat(cloudflare): seminar-ssh用のCNAMEレコードを追加

--- cloudflare/seminar.tf
+++ cloudflare/seminar.tf
@@ -9,3 +9,12 @@ resource "random_password" "seminar" {
   length  = 63
   special = false
 }
+
+resource "cloudflare_dns_record" "seminar-ssh" {
+  zone_id = var.zone_id
+  name    = "seminar-ssh.ncaq.net"
+  type    = "CNAME"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.seminar.id}.cfargotunnel.com"
+  proxied = true
+  ttl     = 1
+}
