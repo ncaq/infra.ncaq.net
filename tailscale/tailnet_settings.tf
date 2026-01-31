40eb89198fd8fc8bbdcdb907d766956d4e698dcc
ncaq <ncaq@ncaq.net>
2026-01-31 15:07:24 +0900

    refactor: 設定したいわけではない値をoptionalにする
    プランの制限で使えないだけで好んで設定しているわけではないので。

--- tailscale/tailnet_settings.tf
+++ tailscale/tailnet_settings.tf
@@ -4,8 +4,5 @@ resource "tailscale_tailnet_settings" "this" {
   devices_key_duration_days                   = 180
   users_approval_on                           = true
   users_role_allowed_to_join_external_tailnet = "admin"
-  network_flow_logging_on                     = false
-  regional_routing_on                         = false
-  posture_identity_collection_on              = false
   https_enabled                               = true
 }
