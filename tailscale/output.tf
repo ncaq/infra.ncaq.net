094ea878fef92d14da597bd268d0cef0b54fe0b3
ncaq <ncaq@ncaq.net>
2026-01-31 16:48:31 +0900

    fix(output): seminarデバイスのIPv6アドレス抽出条件を厳格化
    - seminar_ipv6の抽出条件をTailscaleのULAプレフィックスに限定
    - Tailscaleの仕様に基づき`fd7a:115c:a1e0:`で始まるアドレスのみを対象に変更
    - コード内に公式リファレンスURLをコメントで追加

--- tailscale/output.tf
+++ tailscale/output.tf
@@ -3,7 +3,9 @@ output "seminar_ipv4" {
   value       = one([for addr in data.tailscale_device.this["seminar"].addresses : addr if can(regex("^\\d+\\.\\d+\\.\\d+\\.\\d+$", addr))])
 }
 
+# `fd7a:115c:a1e0::/48`はTailscaleが全tailnetで使用する固定のIPv6 ULAプレフィックス。
+# https://github.com/tailscale/tailscale/blob/main/net/tsaddr/tsaddr.go
 output "seminar_ipv6" {
   description = "Tailscale IPv6 address of the seminar device"
-  value       = one([for addr in data.tailscale_device.this["seminar"].addresses : addr if can(regex(":", addr))])
+  value       = one([for addr in data.tailscale_device.this["seminar"].addresses : addr if can(regex("^fd7a:115c:a1e0:", addr))])
 }
