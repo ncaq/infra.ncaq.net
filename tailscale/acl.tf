c65ce20f29182cb9ac8849e5d3f1929da8fb89a6
ncaq <ncaq@ncaq.net>
2026-01-31 14:28:12 +0900

    feat(tailscale): seminarのsshサーバ機能を禁止
    autogroup:memberタグを付け替えることで特別扱いして、
    デフォルトで許可されていたssh権限を剥奪します。
    Tailscaleの認証だけでsshにアクセス出来るのは嫌なため。
    Tailscaleから接続していてSSH鍵を持っている、
    二段階のガードをかけたい。
    
    - seminarデバイスにtag:serverタグを追加
    - tag:serverのtagOwnersをautogroup:adminに設定
    - ACLにtag:serverとautogroup:member間の通信許可ルールを追加

--- tailscale/acl.tf
+++ tailscale/acl.tf
@@ -1,8 +1,28 @@
 resource "tailscale_acl" "this" {
   acl = jsonencode({
+    tagOwners = {
+      # サーバをautogroupから剥がすことで`tailscale ssh`アクセスを禁止します。
+      # Tailscaleに接続できて、
+      # SSH鍵を持っている。
+      # という二段階のガードを設定するにあたって、
+      # Tailscaleだけでssh出来てしまうと意味がないからです。
+      # クライアントが一時的に立ち上げる場合パスワード認証よりはマシなので、
+      # 全体的には禁止しません。
+      "tag:server" = ["tag:server", "autogroup:admin"],
+    },
     grants = [
       {
         src = ["autogroup:member"],
+        dst = ["autogroup:member"],
+        ip  = ["*"],
+      },
+      {
+        src = ["autogroup:member"],
+        dst = ["tag:server"],
+        ip  = ["*"],
+      },
+      {
+        src = ["tag:server"],
         dst = ["autogroup:member"],
         ip  = ["*"],
       },
--- tailscale/device.tf
+++ tailscale/device.tf
@@ -26,6 +26,12 @@ resource "tailscale_device_key" "this" {
   device_id = data.tailscale_device.this[each.key].id
 }
 
+resource "tailscale_device_tags" "seminar" {
+  device_id  = data.tailscale_device.this["seminar"].id
+  tags       = ["tag:server"]
+  depends_on = [tailscale_acl.this]
+}
+
 resource "tailscale_device_subnet_routes" "seminar" {
   device_id = data.tailscale_device.this["seminar"].id
   routes = [
