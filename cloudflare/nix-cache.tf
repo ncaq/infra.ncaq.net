03624de81f1bbbd46572436c6ba3d6be76122a7c
ncaq <ncaq@ncaq.net>
2026-01-31 17:38:21 +0900

    feat(seminar): atticdエンドポイントをTailscaleに切り替え
    Cloudflare Tunnelは一回のリクエストでのサイズ制限が厳しいため。

--- cloudflare/nix-cache.tf
+++ cloudflare/nix-cache.tf
@@ -1,8 +1,19 @@
-resource "cloudflare_dns_record" "nix_cache" {
+# Tailnet内部からのみアクセス可能なatticバイナリキャッシュ。
+# Tailscale IPは公開しても問題ないため`proxied = false`。
+resource "cloudflare_dns_record" "cache_nix_a" {
   zone_id = var.zone_id
-  name    = "nix-cache.ncaq.net"
-  type    = "CNAME"
-  content = "${cloudflare_zero_trust_tunnel_cloudflared.seminar.id}.cfargotunnel.com"
-  proxied = true
+  name    = "cache.nix.ncaq.net"
+  type    = "A"
+  content = var.seminar_tailscale_ipv4
+  proxied = false
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "cache_nix_aaaa" {
+  zone_id = var.zone_id
+  name    = "cache.nix.ncaq.net"
+  type    = "AAAA"
+  content = var.seminar_tailscale_ipv6
+  proxied = false
   ttl     = 1
 }
