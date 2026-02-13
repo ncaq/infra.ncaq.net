34a02d903cdf803c2da5d3b347f6a4312a11d467
ncaq <ncaq@ncaq.net>
2026-02-13 23:02:01 +0900

    style(tailscale): sort

--- tailscale/tailnet_settings.tf
+++ tailscale/tailnet_settings.tf
@@ -2,7 +2,7 @@ resource "tailscale_tailnet_settings" "this" {
   devices_approval_on                         = false
   devices_auto_updates_on                     = true
   devices_key_duration_days                   = 180
+  https_enabled                               = true
   users_approval_on                           = true
   users_role_allowed_to_join_external_tailnet = "admin"
-  https_enabled                               = true
 }
