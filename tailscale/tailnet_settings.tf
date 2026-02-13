4cece5a3348bbf03d2159d3b06aaffa353ad3bd4
ncaq <ncaq@ncaq.net>
2026-02-13 23:02:18 +0900

    feat(tailscale): acl設定の手動管理をロック
    Terraformなどでの管理を前提にします。

--- tailscale/tailnet_settings.tf
+++ tailscale/tailnet_settings.tf
@@ -1,4 +1,6 @@
 resource "tailscale_tailnet_settings" "this" {
+  acls_external_link                          = "https://github.com/ncaq/infra.ncaq.net"
+  acls_externally_managed_on                  = true
   devices_approval_on                         = false
   devices_auto_updates_on                     = true
   devices_key_duration_days                   = 180
